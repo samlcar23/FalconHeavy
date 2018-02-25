4014ab3fc00a29c1745cff327dd3535a610127c7
Unknown <kernohad@mail.gvsu.edu>
2018-02-24 22:54:40 -0500

    Fixed makeBoosterArray. Also added gitignore
    makeBoosterArray was defined outside of the FalconHeavy class, so when you tried to use the keyword "this" to push to the group, it did not know what this was. So I moved the functino inside the class (which means you need to get rid of the keyword "function") Then to call the function you do "this.makeBoosterArray". Also added gitignore to ignore files .idea files b/c I am using webstorm

--- model/FalconHeavy.js
+++ model/FalconHeavy.js
@@ -22,49 +22,49 @@ class FalconHeavy extends ObjectGroup {
         this.group.push(booster);
 
 
-        //makeBoosterArray(0,0);
+        this.makeBoosterArray(0,0);
         //two other calls for the other two rockets
     }
 
+    makeBoosterArray(x, y){
+
+        //grey
+        var color = vec3.fromValues(.1, .1, .1);
+
+        //temp variables for booster placement
+        var temp1 = 0.4;
+        var temp2 = 0.15;
+
+        //create 9 boosters in formation
+        for (var k = 0; k < 9; k++){
+            let booster = new PolygonalPrism(gl, {
+                topRadius: .05,
+                bottomRadius: .2,
+                numSides: 15,
+                height: .3,
+                topColor: color,
+                bottomColor: color
+            });
+
+            //center booster
+            if(k === 0) {
+                mat4.translate(booster.coordFrame, booster.coordFrame, vec3.fromValues(x, y, 0));
+                //surrounding boosters
+            } else if (k < 5) {
+                mat4.translate(booster.coordFrame, booster.coordFrame, vec3.fromValues(temp1, temp2, 0));
+                temp1 = temp1 * -1;
+                var t = temp1;
+                temp1 = temp2;
+                temp2 = t;
+            } else{
+                mat4.translate(booster.coordFrame, booster.coordFrame, vec3.fromValues(temp2, temp1, 0));
+                temp1 = temp1 * -1;
+                var t = temp1;
+                temp1 = temp2;
+                temp2 = t;
+            }
+
+            this.group.push(booster);
+        }
+    }
 }
-// function makeBoosterArray(x, y){
-//
-//     //grey
-//     var color = vec3.fromValues(.1, .1, .1);
-//
-//     //temp variables for booster placement
-//     var temp1 = 0.4;
-//     var temp2 = 0.15;
-//
-//     //create 9 boosters in formation
-//     for (var k = 0; k < 9; k++){
-//         let booster = new PolygonalPrism(gl, {
-//             topRadius: .05,
-//             bottomRadius: .2,
-//             numSides: 15,
-//             height: .3,
-//             topColor: color,
-//             bottomColor: color
-//         });
-//
-//         //center booster
-//         if(k === 0) {
-//             mat4.translate(booster.coordFrame, booster.coordFrame, vec3.fromValues(x, y, 0));
-//             //surrounding boosters
-//         } else if (k < 5) {
-//             mat4.translate(booster.coordFrame, booster.coordFrame, vec3.fromValues(temp1, temp2, 0));
-//             temp1 = temp1 * -1;
-//             var t = temp1;
-//             temp1 = temp2;
-//             temp2 = t;
-//         } else{
-//             mat4.translate(booster.coordFrame, booster.coordFrame, vec3.fromValues(temp2, temp1, 0));
-//             temp1 = temp1 * -1;
-//             var t = temp1;
-//             temp1 = temp2;
-//             temp2 = t;
-//         }
-//
-//         this.group.push(booster);
-//     }
-//}
