e66b6acc87b49d2c7acd74a0d9af2e28c05d8938
Unknown <kernohad@mail.gvsu.edu>
2018-02-24 23:54:36 -0500

    Added sub groups
    I made it so we create three seperate groups of boosters (leftBoosterGroup, centerBoosterGroup, rightBoosterGroup). This way we can translate the entire group easily inside the FalconHeavy contructor when we build it.

--- model/FalconHeavy.js
+++ model/FalconHeavy.js
@@ -2,31 +2,54 @@
  * Let's make a rocket
  */
 class FalconHeavy extends ObjectGroup {
+
     constructor(gl) {
         super(gl);
 
         var color = vec3.fromValues(.1, .1, .1);
 
-        //temp example of the booster
-        let booster = new PolygonalPrism(gl, {
-            topRadius: .05,
-            bottomRadius: .2,
-            numSides: 15,
-            height: .3,
-            topColor: color,
-            bottomColor: color
-        });
+        // //temp example of the booster
+        // let booster = new PolygonalPrism(gl, {
+        //     topRadius: .05,
+        //     bottomRadius: .2,
+        //     numSides: 15,
+        //     height: .3,
+        //     topColor: color,
+        //     bottomColor: color
+        // });
+        //
+        // mat4.translate(booster.coordFrame, booster.coordFrame, vec3.fromValues(0, 0, 0));
+        //
+        // this.group.push(booster);
 
-        mat4.translate(booster.coordFrame, booster.coordFrame, vec3.fromValues(0, 0, 0));
 
-        this.group.push(booster);
+        let leftBoosterGroup = new ObjectGroup(gl);
+        let centerBoosterGroup = new ObjectGroup(gl);
+        let rightBoosterGroup = new ObjectGroup(gl);
+
+        leftBoosterGroup = this.makeBoosterArray(0,0);
+        centerBoosterGroup = this.makeBoosterArray(0,0);
+        rightBoosterGroup = this.makeBoosterArray(0,0);
+
+        /*
+          TODO: We should consider adding the leftBoosterGroup to a group we make for the leftCylinder or something.
+          TODO:-- This way we would have only 3 groups that make up the main group
+        */
+        // Push all three booster groups to main Falcon Heavy Group (this).
+        this.group.push(leftBoosterGroup, centerBoosterGroup, rightBoosterGroup);
+
+        // Translate booster groups so all in a line side by side
+        mat4.translate(leftBoosterGroup.coordFrame, leftBoosterGroup.coordFrame, vec3.fromValues(-1, 1, 0));
+        mat4.translate(centerBoosterGroup.coordFrame, centerBoosterGroup.coordFrame, vec3.fromValues(0, 0, 0));
+        mat4.translate(rightBoosterGroup.coordFrame, rightBoosterGroup.coordFrame, vec3.fromValues(1, -1, 0));
 
 
-        this.makeBoosterArray(0,0);
-        //two other calls for the other two rockets
     }
 
     makeBoosterArray(x, y){
+
+        // Group to hold all boosters from this call
+        let boosterGroup = new ObjectGroup(gl);
 
         //grey
         var color = vec3.fromValues(.1, .1, .1);
@@ -64,7 +87,13 @@ class FalconHeavy extends ObjectGroup {
                 temp2 = t;
             }
 
-            this.group.push(booster);
+            // Push the single booster to the booster group
+            boosterGroup.group.push(booster);
         }
+
+        // Return the whole group to me manipulated later.
+        return boosterGroup;
     }
+
+    //TODO: Create a function that makes the Rocket cylinders. Maybe add all the little things to them in this function too?
 }
